764fca80df2c111799fee13556accedb874712a7
hia1394 <a4737465@naver.com>
2025-12-29 16:27:00 +0900

    local

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://58.127.241.84:8090",
+  url: "http://10.0.147.41:8090",
   realm: "ctrlf",
   clientId: "web-app",
 });

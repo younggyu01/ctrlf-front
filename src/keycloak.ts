405bc8abf7af0c8108076ce30322f9ae1c338e67
hia1394 <a4737465@naver.com>
2025-12-30 12:14:07 +0900

    test

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://192.168.0.112:8090",
+  url: "http://keycloak:8090",
   realm: "ctrlf",
   clientId: "web-app",
 });

d98ed0d3b81664e6ed219774df32ca659cd0be84
hia1394 <a4737465@naver.com>
2025-12-30 16:54:53 +0900

    test

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://k8s-argocd-keycloak-446de99eb5-6274bddb4987261a.elb.ap-northeast-2.amazonaws.com:8080",
+  url: "http://k8s-argocd-keycloak-446de99eb5-6274bddb4987261a.elb.ap-northeast-2.amazonaws.com:8090",
   realm: "ctrlf",
   clientId: "web-app",
 });

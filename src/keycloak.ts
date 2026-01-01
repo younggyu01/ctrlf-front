99b7d25ce8781cff96515049fdf7a041075f2ddc
hia1394 <a4737465@naver.com>
2026-01-02 00:25:13 +0900

    test

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -5,7 +5,7 @@ const KEYCLOAK_REALM = import.meta.env.VITE_KEYCLOAK_REALM ?? "ctrlf";
 const KEYCLOAK_CLIENT_ID = import.meta.env.VITE_KEYCLOAK_CLIENT_ID ?? "web-app";
 
 const keycloak = new Keycloak({
-  url: "http://a673ff9a28cd54774ab60417539c4bfa-1139660103.ap-northeast-2.elb.amazonaws.com:8090",
+  url: "http://k8s-argocd-ctrlfing-24d9f59895-1823150137.ap-northeast-2.elb.amazonaws.com/auth",
   realm: "ctrlf",
   clientId: "web-app",
 });

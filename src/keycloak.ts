b78a346b7c6e79fd1177a87910c8c5e9d2ae6ac3
hia1394 <a4737465@naver.com>
2026-01-02 09:56:46 +0900

    te

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,13 +1,13 @@
 import Keycloak from "keycloak-js";
 
-const KEYCLOAK_URL = import.meta.env.VITE_KEYCLOAK_URL ?? "http://k8s-argocd-ctrlfing-24d9f59895-1823150137.ap-northeast-2.elb.amazonaws.com/auth:8090";
+const KEYCLOAK_URL = import.meta.env.VITE_KEYCLOAK_URL ?? "http://k8s-argocd-ctrlfing-24d9f59895-1823150137.ap-northeast-2.elb.amazonaws.com/auth";
 const KEYCLOAK_REALM = import.meta.env.VITE_KEYCLOAK_REALM ?? "ctrlf";
 const KEYCLOAK_CLIENT_ID = import.meta.env.VITE_KEYCLOAK_CLIENT_ID ?? "web-app";
 
 const keycloak = new Keycloak({
-  url: "http://k8s-argocd-ctrlfing-24d9f59895-1823150137.ap-northeast-2.elb.amazonaws.com/auth",
-  realm: "ctrlf",
-  clientId: "web-app",
+  url: VITE_KEYCLOAK_URL,
+  realm: VITE_KEYCLOAK_REALM,
+  clientId: VITE_KEYCLOAK_CLIENT_ID,
 });
 
 export default keycloak;

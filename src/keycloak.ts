26d47240554eb79864466926222f05c8e1a85471
hia1394 <a4737465@naver.com>
2026-01-05 17:18:17 +0900

    test

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,6 +1,6 @@
 import Keycloak from "keycloak-js";
 
-const KEYCLOAK_URL = import.meta.env.VITE_KEYCLOAK_URL ?? "https://8bang-ctrlf.com/auth";
+const KEYCLOAK_URL = import.meta.env.VITE_KEYCLOAK_URL ?? "https://8bang-ctrlf.com:auth";
 const KEYCLOAK_REALM = import.meta.env.VITE_KEYCLOAK_REALM ?? "ctrlf";
 const KEYCLOAK_CLIENT_ID = import.meta.env.VITE_KEYCLOAK_CLIENT_ID ?? "web-app";
 
